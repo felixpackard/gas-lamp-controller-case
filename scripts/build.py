89b9991bab9d07af456956511e43711f38fa4fc7
Felix Packard <fjpackard@gmail.com>
2021-02-20 17:56:28 +0000

    Improvements to build script

--- scripts/build.py
+++ scripts/build.py
@@ -2,17 +2,35 @@
 import os
 import configparser
 
-# Read config
+# Globals
 config = configparser.ConfigParser()
-config.read("config.ini")
 
-# Create out directory if it doesn't exist
-os.chdir("../")
-if not os.path.exists("out"):
-    os.mkdir("out")
 
-# Build all modules in parts/
-for file in os.listdir("parts"):
-    filename = os.path.splitext(file)[0]
-    openscad_path = config.get("build", "openscad_path")
-    os.system(f"{openscad_path} -o out/{filename}.stl parts/{file}")
+def setup() -> None:
+    # Read config
+    config.read("config.ini")
+
+    # Create out directory if it doesn't exist
+    os.chdir("../")
+    if not os.path.exists("out"):
+        os.mkdir("out")
+
+
+def parts_to_stl() -> None:
+    # Build all modules in parts/
+    for file in os.listdir("parts"):
+        filename = os.path.splitext(file)[0]
+
+        print(f"[BUILDING] parts/{file} → out/{filename}.stl ...", end="")
+
+        openscad_path = config.get("build", "openscad_path")
+        os.system(f"{openscad_path} -o out/{filename}.stl parts/{file}")
+
+        print(" [DONE]")
+
+    print("\nAll parts have finished building. Check the output for errors.")
+
+
+if __name__ == "__main__":
+    setup()
+    parts_to_stl()
